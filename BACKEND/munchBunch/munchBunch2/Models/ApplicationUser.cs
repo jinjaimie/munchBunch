1bf523bfa45672b68a4c8463a3a88002118ca1e8
996JVT <jinjaimie12@gmail.com>
2019-10-12 21:08:21 -0700

    backend semi working

---
+++ BACKEND/munchBunch/munchBunch2/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace munchBunch.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column(TypeName = "nvarchar(150)")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar(150)")]
+        public string LastName { get; set; }
+    }
+}
